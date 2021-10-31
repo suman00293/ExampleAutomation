c5ce6d2f99dcd0c61d67fef5777db42965a91156
suman00293 <chatterjeesuman002@gmail.com>
2021-10-31 09:03:01 +0530

    Automation Frameworl level2

---
+++ ActiTime_AutomationLevel2/src/com/sgtesting/actitime/tests/Projects.java
@@ -0,0 +1,73 @@
+package com.sgtesting.actitime.tests;
+
+import org.openqa.selenium.WebDriver;
+
+public class Projects {
+	
+	/**
+	 * TestCase ID:
+	 * Module Name:
+	 * Created By:
+	 * Reviewed By:
+	 * Modified By:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void createProject(WebDriver oBrowser)
+	{
+		try
+		{
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * TestCase ID:
+	 * Module Name:
+	 * Created By:
+	 * Reviewed By:
+	 * Modified By:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void modifyProject(WebDriver oBrowser)
+	{
+		try
+		{
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * TestCase ID:
+	 * Module Name:
+	 * Created By:
+	 * Reviewed By:
+	 * Modified By:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void deleteProject(WebDriver oBrowser)
+	{
+		try
+		{
+			
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+}
